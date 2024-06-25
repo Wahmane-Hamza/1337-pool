053f0bbb84e70fac4bbbebc2c0e8096dbe646727
hamza wahmane <hamza.wahm5@gmail.com>
2024-06-25 21:55:22 +0100

    9

---
+++ c01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/19 12:15:29 by hwahmane          #+#    #+#             */
+/*   Updated: 2023/08/19 14:15:37 by hwahmane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	j;
+	int	cup;
+
+	j = 0;
+	while (j < size - 1)
+	{
+		i = 0;
+		while (i < size - 1)
+		{
+			if (tab[i] > tab[i + 1])
+			{
+				cup = tab[i];
+				tab[i] = tab[i + 1];
+				tab[i + 1] = cup;
+			}
+			i++;
+		}
+	j++;
+	}
+}
