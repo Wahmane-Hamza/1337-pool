f1abeb61879e67aef4290df4c4ebd956d202cc49
hamza wahmane <hamza.wahm5@gmail.com>
2024-06-27 20:18:30 +0100

    4

---
+++ c03/ex03/ft_strncat.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/21 17:19:50 by hwahmane          #+#    #+#             */
+/*   Updated: 2023/08/24 14:10:06 by hwahmane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	while (dest[i])
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] && j < nb)
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	dest[j + i] = '\0';
+	return (dest);
+}
