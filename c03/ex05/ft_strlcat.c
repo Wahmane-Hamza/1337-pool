166827fb1c33d25117fba87f14f5a3b61e095078
hamza wahmane <hamza.wahm5@gmail.com>
2024-06-27 20:18:38 +0100

    6

---
+++ c03/ex05/ft_strlcat.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/22 10:12:21 by hwahmane          #+#    #+#             */
+/*   Updated: 2023/08/24 14:30:22 by hwahmane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	j;
+	unsigned int	k;
+
+	i = 0;
+	while (dest[i])
+	{
+		i++;
+	}
+	k = 0;
+	while (src[k])
+	{
+		k++;
+	}
+	if (size == 0 || size <= i)
+		return (size + k);
+	j = 0;
+	while (src[j] != '\0' && j < size - i - 1)
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	dest[j + i] = '\0';
+	return (i + k);
+}
