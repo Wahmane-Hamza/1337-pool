c53f5c520f3ef7007cd6f93493320f9c4597e97c
hamza wahmane <hamza.wahm5@gmail.com>
2024-06-30 19:47:20 +0100

    5

---
+++ c05/ex04/ft_fibonacci.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/24 15:24:49 by hwahmane          #+#    #+#             */
+/*   Updated: 2023/08/27 15:38:50 by hwahmane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_fibonacci(int index)
+{
+	if (index < 0)
+	{
+		return (-1);
+	}
+	if (index < 2)
+	{
+		return (index);
+	}
+	else
+	{
+		return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+	}
+}
