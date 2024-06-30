48be8e2192e9fac13de16917b68451582e1a32f3
hamza wahmane <hamza.wahm5@gmail.com>
2024-06-30 19:47:11 +0100

    2

---
+++ c05/ex01/ft_recursive_factorial.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/23 19:09:30 by hwahmane          #+#    #+#             */
+/*   Updated: 2023/08/26 21:26:00 by hwahmane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb < 0)
+	{
+		return (0);
+	}
+	if (nb == 0 || nb == 1)
+	{
+		return (1);
+	}
+	return (nb * ft_recursive_factorial(nb - 1));
+}
