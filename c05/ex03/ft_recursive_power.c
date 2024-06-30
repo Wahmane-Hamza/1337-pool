004a197d342eff12a04a30c624d39d68cdc63c87
hamza wahmane <hamza.wahm5@gmail.com>
2024-06-30 19:47:16 +0100

    4

---
+++ c05/ex03/ft_recursive_power.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/24 15:18:22 by hwahmane          #+#    #+#             */
+/*   Updated: 2023/08/27 15:40:06 by hwahmane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_recursive_power(int nb, int power)
+{
+	if (power < 0)
+	{
+		return (0);
+	}
+	if (power > 0)
+	{
+		return (nb * ft_recursive_power(nb, power - 1));
+	}
+	return (1);
+}
