63356da2f8bb59feaade3b393f254f8a4856c3ca
hamza wahmane <hamza.wahm5@gmail.com>
2024-07-02 17:19:10 +0100

    1

---
+++ c07/ex00/ft_strdup.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/28 19:53:03 by hwahmane          #+#    #+#             */
+/*   Updated: 2023/09/02 16:22:17 by hwahmane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int	len(char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*cpy(char *src, char	*dest)
+{
+	int	i;
+
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*p;
+
+	p = malloc(len(src) + 1);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	cpy(src, p);
+	return (p);
+}
