5dc4c0a8cb03043dd5b8ee6c5e7de200cecbcae7
hamza wahmane <hamza.wahm5@gmail.com>
2024-07-12 17:00:11 +0100

    fix

--- c05/ex02/ft_iterative_power.c
+++ c05/ex02/ft_iterative_power.c
@@ -10,8 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-
 int	ft_iterative_power(int nb, int power)
 {
 	int	i;
