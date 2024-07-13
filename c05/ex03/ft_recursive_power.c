2a4bcd6e14284aa9bce574245ea70f47712e5390
hamza wahmane <hamza.wahm5@gmail.com>
2024-07-13 18:12:36 +0100

    fix

--- c05/ex03/ft_recursive_power.c
+++ c05/ex03/ft_recursive_power.c
@@ -10,8 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-
 int	ft_recursive_power(int nb, int power)
 {
 	if (power < 0)
