c052e738ba7b844a28dc700f47f6888fc4685777
hamza wahmane <hamza.wahm5@gmail.com>
2024-07-23 21:39:28 +0100

    fix

--- c07/ex01/ft_range.c
+++ c07/ex01/ft_range.c
@@ -10,9 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <stdio.h>
-
 int	*ft_range(int min, int max)
 {
 	int	*result;
