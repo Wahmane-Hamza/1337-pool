ab305f44eef1f33fe47c07322a3c06fdfbd73690
hamza wahmane <hamza.wahm5@gmail.com>
2024-07-25 22:11:28 +0100

    fix

--- c07/ex03/ft_strjoin.c
+++ c07/ex03/ft_strjoin.c
@@ -10,9 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
-
 int	lenstr(int size, char **str)
 {
 	int	i;
