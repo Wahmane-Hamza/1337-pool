d4d7ed341992b190131a92df4fd78ef1c6d44501
hamza wahmane <hamza.wahm5@gmail.com>
2024-08-10 14:42:09 +0100

    fiiiix

--- c03/ex05/ft_strlcat.c
+++ c03/ex05/ft_strlcat.c
@@ -10,8 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
 	unsigned int	i;
